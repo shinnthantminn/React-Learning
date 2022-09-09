3adc7315f35e40a94f43ed2645778edead1d6446
shinnthantminn <shinthantmin32@gmail.com>
2022-09-09 16:08:35 +0630

    add

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { BiTrash, BiEdit } from "react-icons/bi";
+
+const App = () => {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleChange = (e) => {
+    setText(e.currentTarget.value);
+  };
+
+  const handleClick = () => {
+    if (text != "") {
+      const data = {
+        id: uuidv4(),
+        text,
+      };
+      setList((pre) => [...pre, data]);
+      setText("");
+    } else {
+      alert("Error");
+    }
+  };
+
+  const handleDelete = (e) => {
+    setList((preState) => preState.filter((i) => i.id !== e));
+  };
+
+  const handleEdit = (id) => {
+    const value = prompt("are you sure you want to edit");
+    if (value !== "") {
+      setList((pre) =>
+        pre.map((i) => (i.id === id ? { id: uuidv4(), text: value } : i))
+      );
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="header">CRUD Todo</h1>
+      <div className="inputContainer">
+        <input
+          value={text}
+          onChange={handleChange}
+          className="input"
+          type="text"
+        />
+        <button onClick={handleClick}>Add Goal</button>
+      </div>
+      <div>
+        {list.map((i) => (
+          <div key={i.id} className="listContainer">
+            <p> {i.text}</p>
+            <div>
+              <BiTrash
+                onClick={() => {
+                  handleDelete(i.id);
+                }}
+              />
+              <BiEdit
+                onClick={() => {
+                  handleEdit(i.id);
+                }}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
